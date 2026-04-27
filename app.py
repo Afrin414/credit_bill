be424875ebf4caed8fef952021741b67dd7a2cae
afrin shaikh <afrinshaikh1272@gmail.com>
2026-04-27 07:19:36 +0000

    completed

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
 
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
-scaler = pickle.load(open('/workspaces/credit_bill/notebook/scaler.pkl', 'rb'))
+import os
+import pickle
+
+scaler_path = os.path.join(os.path.dirname(__file__), 'scaler.pkl')
+
+with open(scaler_path, 'rb') as f:
+    scaler = pickle.load(f)
 
 st.title("💳 Credit Card Default Prediction")
 
