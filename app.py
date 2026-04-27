3f641044e8c89c6cf6c3617c7f10ec6a510e6234
afrin shaikh <afrinshaikh1272@gmail.com>
2026-04-27 07:10:40 +0000

    completed

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import pandas as pd
 import pickle
 
 # Load model & scaler
-model = pickle.load(open('/workspaces/credit_bill/notebook/model.pkl', 'rb'))
+import os
+import pickle
+
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
 scaler = pickle.load(open('/workspaces/credit_bill/notebook/scaler.pkl', 'rb'))
 
 st.title("💳 Credit Card Default Prediction")
